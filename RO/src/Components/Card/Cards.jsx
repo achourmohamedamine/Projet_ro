c538af21a393707d9de70d106869e023628214d4
ACER <achour.mohamedamine@insat.ucar.tn>
2024-12-18 13:27:55 +0100

    Message

--- RO/src/Components/Card/Cards.jsx
+++ RO/src/Components/Card/Cards.jsx
@@ -2,10 +2,24 @@ import React from 'react';
 import { Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
 import '/src/Components/Card/Cards.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route,useNavigate } from 'react-router-dom';
+
 
 
 
 function Cards  ()  {
+  const navigate=useNavigate();
+ 
+  const handleClick =()=> {
+    
+      navigate('/Problem1');
+  }
+  
+  const handleClick2 =()=> {
+    navigate('/Problem2');
+  }
+
+
   return ( <>
     
     <Row>
@@ -13,14 +27,14 @@ function Cards  ()  {
         <Card body>
           <CardTitle>Special Title Treatment</CardTitle>
           <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-          <Button>Go somewhere</Button>
+          <Button onClick={handleClick}>Go to Problem 1 </Button>
         </Card>
       </Col>
       <Col className="sm=6">
         <Card body>
           <CardTitle>Special Title Treatment</CardTitle>
           <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-          <Button>Go somewhere</Button>
+          <Button onClick={handleClick2}>Go to Problem 2 </Button>
         </Card>
       </Col>
     </Row>
